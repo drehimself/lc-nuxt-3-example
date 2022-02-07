8f2c96d8a2e564d32b87f8d559814d46644a86ab
Andre Madarang <andre_madarang@hotmail.com>
2022-02-07 13:17:50 -0500

    Data fetching

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,48 @@
     x: {{ x }}
     y: {{ y }}
   </div>
+  <div>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+  <div>
+    <ul>
+      <li v-for="user in users2" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+
+  <div>
+    <ul>
+      <li v-for="user in users3" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+
+  <div>
+    {{ user.name }}
+  </div>
 </div>
 </template>
 
 <script setup>
   const {x, y} = useMouse()
+  const users = ref([])
+
+  onMounted(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => users.value = data)
+  })
+
+  const { data: users2 } = await useAsyncData('users2', () => $fetch('https://jsonplaceholder.typicode.com/users'))
+
+  const { data : users3 } = await useFetch('https://jsonplaceholder.typicode.com/users')
+
+  const { data : user } = await useFetch('https://jsonplaceholder.typicode.com/users/1' ,{ pick: ['id', 'name', 'email'] })
 </script>
